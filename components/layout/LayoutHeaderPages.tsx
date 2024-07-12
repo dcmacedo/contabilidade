0078e52d99ee6eedc346a163de8d15ad5d20625d
dcmacedo <dcotrimmacedo@gmail.com>
2024-07-12 18:33:31 -0300

    :sparkles:feat: First commit new site in Angular

--- components/layout/LayoutHeaderPages.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from 'react'
-
-type Props = {
-  children?: ReactNode
-}
-
-const LayoutHeaderPages = ({ children }: Props) => <header>{children}</header>
-
-export default LayoutHeaderPages
