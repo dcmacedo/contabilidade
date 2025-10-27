06166852d689d570e8a23eed254aece848ac030c
Danilo Cotrim Macedo <dcotrimmacedo@gmail.com>
2025-10-27 17:17:45 -0300

    :sparkles:feat: Adicionar Google Ads

--- app/GoogleAnalytics.tsx
+++ app/GoogleAnalytics.tsx
@@ -1,48 +1,55 @@
 "use client";
 
-
 import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 import Script from "next/script";
 
-
-const GA_MEASUREMENT_ID = "G-WHH5NFS1H3";
-
+// IDs de medição
+const GA_MEASUREMENT_ID = "G-WHH5NFS1H3"; // GA4
+const ADS_ID = "AW-405311343"; // Google Ads
 
 export default function GoogleAnalytics() {
-const pathname = usePathname();
-const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
+  useEffect(() => {
+    if (typeof window === "undefined" || !window.gtag || !pathname) return;
 
-useEffect(() => {
-if (!pathname || typeof window === "undefined" || !window.gtag) return;
-const url =
-window.location.origin +
-pathname +
-(searchParams?.toString() ? `?${searchParams}` : "");
-window.gtag("event", "page_view", {
-page_location: url,
-page_path: pathname,
-page_title: document.title,
-});
-}, [pathname, searchParams]);
+    const url =
+      window.location.origin +
+      pathname +
+      (searchParams?.toString() ? `?${searchParams}` : "");
 
+    // Rastreia visualização de página
+    window.gtag("event", "page_view", {
+      page_location: url,
+      page_path: pathname,
+      page_title: document.title,
+    });
+  }, [pathname, searchParams]);
 
-return (
-<>
-<Script
-src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-strategy="afterInteractive"
-/>
-<Script id="ga-init" strategy="afterInteractive">
-{`
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-window.gtag = gtag;
-gtag('js', new Date());
-gtag('config', '${GA_MEASUREMENT_ID}', { send_page_view: false });
-`}
-</Script>
-</>
-);
+  return (
+    <>
+      {/* Script principal do gtag */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+
+      {/* Inicialização do GA4 + Ads */}
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          // GA4
+          gtag('config', '${GA_MEASUREMENT_ID}', { send_page_view: false });
+
+          // Google Ads
+          gtag('config', '${ADS_ID}');
+        `}
+      </Script>
+    </>
+  );
 }
